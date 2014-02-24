45dba7f7a3726ffa4f12c31076e18dc4f58e358b
hughhopkins <hugh.hopkins@gmail.com>
2014-02-24 18:12:48 +0000

    updating anlaytics plus ecom

--- client/views/eshop.js
+++ client/views/eshop.js
@@ -2,16 +2,39 @@ Template.eshop.events({
   'click input.eshop1': function () {
     alert("Boom it's coming!");
     console.log("eshop1");
+    analytics.track('Completed Order', {
+      id: '50314b8e9bcf000000000000',
+      total: 31.39,
+      shipping: 3.47,
+      tax: 2.93,
+      products: [{
+        id: '507f1f77bcf86cd799439011',
+        sku: '45790-32',
+        name: 'Monopoly: 3rd Edition',
+        price: 18.99
+  }, {
+        id: '505bd76785ebb509fc183733',
+        sku: '46493-32',
+        name: 'Uno Card Game',
+        price: 6.00
+  }]
+});
+    analytics.track('Viewed Product', {
+      id: '507f1f77bcf86cd799439011',
+      sku: '45790-32',
+      name: 'Porsche',
+      price: 999.90
+});
     _gs('transaction', 'Transaction ID5', {
   // revenue: 5.99,
   // quantity: 10,
   track: true
 }, [
   {
-    name: 'Beer',
-    price: 2,
-    quantity: 3,
-    category: 'Booze'
+    name: 'Porsche Targa',
+    price: 100000,
+    quantity: 1,
+    category: 'Cars and Stuff'
   },
 ]);
     mixpanel.track("eshop1");
